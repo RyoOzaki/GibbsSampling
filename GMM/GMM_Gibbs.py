af892dc3728040c5f08108f8e041cf888a544b41
RyoOzaki <ryo.ozaki@em.ci.ritsumei.ac.jp>
2018-11-01 08:54:40 -0700

    Updated the all document and source

--- GMM/GMM_Gibbs.py
+++ GMM/GMM_Gibbs.py
@@ -1,5 +1,6 @@
-#%% Import parts.
+#%% Import.
 # %matplotlib inline
+from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
@@ -7,7 +8,7 @@ from matplotlib import patches
 import scipy.stats as stats
 from tqdm import trange
 
-#%% Define functions.
+#%% Functions.
 def counting(datas, size, out=None):
     if out is None:
         cnt = np.zeros(size, dtype=int)
@@ -44,10 +45,17 @@ def plot_covariance(axes, mean, covariance, scale=3.0, color=None):
 def plot_datas(axes, datas, color=None, mark="+"):
     return axes.scatter(datas[:,0], datas[:,1], marker=mark, c=color, zorder=1)
 
-#%% Initialize truth parameters.
+#%% Make a figure dir and delete old files.
+if not Path("fig").exists():
+    Path("fig").mkdir()
+imgs = Path("fig").glob("*.png")
+for img in imgs:
+    img.unlink()
+
+#%% Truth parameters.
 K_truth = 3
 D = 2
-N = 300
+N = 3000
 ITERATION = 150
 pi_truth = np.array([0.7, 0.2, 0.1])
 mean_truth = np.array([[3.0, 0.0], [-3.0, 0.0], [0.0, 5.0]])
@@ -59,7 +67,7 @@ for i in range(K_truth):
 #%% Plot color initializeton.
 colors = np.array(["tab:blue", "tab:orange", "tab:pink", "tab:green", "tab:red", "tab:cyan"])
 
-#%% Generate datas.
+#%% Observations.
 datas = np.empty((N, D))
 hidden_state_truth = np.random.choice(K_truth, size=N, p=pi_truth)
 for n in range(N):
@@ -90,79 +98,76 @@ ylim = (0.2 * -height + ylim[0], 0.2 * height + ylim[1])
 ax_data.set_xlim(xlim)
 ax_data.set_ylim(ylim)
 #plt.show()
-plt.savefig("tmp/truth.png")
+plt.savefig("fig/truth.png")
 
-#%% Initialize hyper parameters.
+#%% Hyper parameters.
 K = 4
 alpha_0 = np.ones(K)
 mu_0 = np.zeros((K, D))
-Sigma_0 = np.empty((K, D, D))
-for k in range(K):
-    Sigma_0[k] = np.identity(D)
+lambda_0 = np.ones(K)
 nu_0 = np.ones(K) * D
 Psi_0 = np.empty((K, D, D))
 for k in range(K):
     Psi_0[k] = np.identity(D)
 
-#%% Initialize local parameters.
+#%% Local parameters.
 pi = stats.dirichlet.rvs(alpha_0)[0]
 mu = np.empty((K, D))
 Sigma = np.empty((K, D, D))
 for k in range(K):
-    mu[k] = stats.multivariate_normal.rvs(mean=mu_0[k], cov=Sigma_0[k])
     Sigma[k] = stats.invwishart.rvs(df=nu_0[k], scale=Psi_0[k])
+    mu[k] = stats.multivariate_normal.rvs(mean=mu_0[k], cov=(Sigma[k] / lambda_0[k]))
 
-#%% Define tmp variables.
-k_vec = np.zeros(K)
-D_zeros = np.matrix(np.zeros(D))
-p_yi = np.empty((N, K))
+#%% Temporary variables.
+p_xi = np.empty((N, K))
+
+#%% Latent variables
 hidden_state = np.zeros(N, dtype=int)
-hidden_state_count = np.zeros(K, dtype=int)
 
-#%% Start Gibbs sampling.
+#%% Gibbs sampling iteration.
 for t in trange(ITERATION):
 
     # Calculate phase.
     for k in range(K):
-        p_yi[:,k] = stats.multivariate_normal.pdf(datas, mean=mu[k], cov=Sigma[k])
-    p_yi *= pi
+        p_xi[:,k] = stats.multivariate_normal.pdf(datas, mean=mu[k], cov=Sigma[k])
+    p_xi *= pi
 
     # Resampling labels.
     for n in range(N):
-        k_vec[:] = p_yi[n]
-        k_vec /= k_vec.sum()
-        hidden_state[n] = np.random.choice(K, p=k_vec)
-    counting(hidden_state, K, out=hidden_state_count)
+        pi_ast = p_xi[n] / p_xi[n].sum()
+        hidden_state[n] = np.random.choice(K, p=pi_ast)
+    m = counting(hidden_state, K)
 
-    # Resampling parameters phase.
     # Resampling pi.
-    alpha_hat = alpha_0 + hidden_state_count
-    pi[:] = stats.dirichlet.rvs(alpha_hat)
+    alpha_ast = alpha_0 + m
+    pi[:] = stats.dirichlet.rvs(alpha_ast)
 
     # Resampling mu_k and Sigma_k
     for k in range(K):
-        data_k = np.matrix(datas[hidden_state == k])
-        if data_k.shape[0] is 0:
-            data_k = D_zeros
-        m_k = hidden_state_count[k]
-        sum_data_k = data_k.sum(axis=0)
+        if m[k] == 0:
+            data_k = np.matrix(np.zeros(D))
+        else:
+            data_k = np.matrix(datas[hidden_state == k])
+
+        # Calculate Temporary variables.
+        mean_of_data_k = data_k.mean(axis=0)
+        S_k = np.dot((data_k - mean_of_data_k).T, data_k - mean_of_data_k)
 
         # Resampling Sigma_k.
-        tmp = np.matrix(data_k - mu[k])
-        Psi_hat = tmp.T.dot(tmp) + Psi_0[k]
-        nu_hat = m_k + nu_0[k]
-        Sigma[k] = stats.invwishart.rvs(df=nu_hat, scale=Psi_hat)
+        nu_ast = m[k] + nu_0[k]
+        Psi_ast = S_k + Psi_0[k] + (m[k] * lambda_0[k])/(m[k] + lambda_0[k]) * np.dot((mean_of_data_k - mu_0[k]).T, mean_of_data_k - mu_0[k])
+        Sigma[k] = stats.invwishart.rvs(df=nu_ast, scale=Psi_ast)
 
         # Resampling mu_k.
-        Sigma_hat = np.array((m_k * np.matrix(Sigma[k]).I + np.matrix(Sigma_0[k]).I).I)
-        mu_hat = np.array((sum_data_k.dot(np.matrix(Sigma[k]).I) + mu_0[k].dot(np.matrix(Sigma_0[k]).I)).dot(Sigma_hat))[0]
-        mu[k] = stats.multivariate_normal.rvs(mean=mu_hat, cov=Sigma_hat)
+        lambda_ast = m[k] + lambda_0[k]
+        mu_ast = ((m[k] * mean_of_data_k + lambda_0[k] * mu_0[k]) / (m[k] + lambda_0[k])).A1
+        mu[k] = stats.multivariate_normal.rvs(mean=mu_ast, cov=(Sigma[k] / lambda_ast))
 
     # Save animation phase.
     plt.cla()
     plot_mixingrate(ax_pi, pi, colors=colors[:K])
     for k in range(K):
-        if hidden_state_count[k] != 0:
+        if m[k] != 0:
             data_k = datas[hidden_state == k]
             plot_datas(ax_data, data_k, color=colors[k])
         plot_mean(ax_data, mu[k], color=colors[k])
@@ -170,12 +175,12 @@ for t in trange(ITERATION):
     ax_data.set_xlim(xlim)
     ax_data.set_ylim(ylim)
     # plt.pause(0.1)
-    plt.savefig("tmp/frame{0:03d}.png".format(t+1))
+    plt.savefig("fig/frame{0:03d}.png".format(t+1))
 
 plt.cla()
 plot_mixingrate(ax_pi, pi, colors=colors[:K])
 for k in range(K):
-    if hidden_state_count[k] != 0:
+    if m[k] != 0:
         data_k = datas[hidden_state == k]
         plot_datas(ax_data, data_k, color=colors[k])
     plot_mean(ax_data, mu[k], color=colors[k])
@@ -183,4 +188,4 @@ for k in range(K):
 ax_data.set_xlim(xlim)
 ax_data.set_ylim(ylim)
 # plt.show()
-plt.savefig("tmp/final.png")
+plt.savefig("fig/final.png")
--- GMM/make_gif.sh
+++ GMM/make_gif.sh
@@ -4,4 +4,4 @@ DELAY=10
 TOP_DELAY=100
 REAR_DELAY=100
 
-convert -layers optimize -loop 0 -delay ${TOP_DELAY} tmp/truth.png -delay ${DELAY} tmp/frame*.png -delay ${REAR_DELAY} tmp/final.png result.gif
+convert -layers optimize -loop 0 -delay ${TOP_DELAY} fig/truth.png -delay ${DELAY} fig/frame*.png -delay ${REAR_DELAY} fig/final.png result.gif
